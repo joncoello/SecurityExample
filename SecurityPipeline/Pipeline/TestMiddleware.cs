67cb9ac963cd557ee56ea2a05b196ac33684d330
Jon Coello <joncoello@hotmail.com>
2015-11-10 21:41:28 +0000

    setting the principal

--- SecurityPipeline/Pipeline/TestMiddleware.cs
+++ SecurityPipeline/Pipeline/TestMiddleware.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -17,6 +18,11 @@ namespace SecurityPipeline.Pipeline
         public async Task Invoke(IDictionary<string, object> env) {
 
             var context = new OwinContext(env);
+
+            // authentcation
+            context.Request.User = 
+                new GenericPrincipal(new GenericIdentity("dom"), new string[] { });
+
             Helper.Write("Middleware", context.Request.User);
 
             await _next(env);
